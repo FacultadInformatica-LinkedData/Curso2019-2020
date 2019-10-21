73f6a51dd983e428ce9c055b9641d3537baac0ee
Montserrat Silva <40004042+MonneSilva@users.noreply.github.com>
2019-10-21 18:30:25 +0200

    Project Java

---
+++ HandsOn/Group18/ServletsEx/src/java/NewServlet.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author monts
+ */
+@WebServlet(urlPatterns = {"/NewServlet"})
+public class NewServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet NewServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		// set response headers
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		
+		// create HTML form
+		PrintWriter writer = response.getWriter();
+		writer.append("<!DOCTYPE html>\r\n")
+			  .append("<html>\r\n")
+			  .append("		<head>\r\n")
+			  .append("			<title>Form input</title>\r\n")
+			  .append("		</head>\r\n")
+			  .append("		<body>\r\n")
+			  .append("			<form action=\"welcome\" method=\"POST\">\r\n")
+			  .append("				Enter your name: \r\n")
+			  .append("				<input type=\"text\" name=\"user\" />\r\n")
+			  .append("				<input type=\"submit\" value=\"Submit\" />\r\n")
+			  .append("			</form>\r\n")
+			  .append("		</body>\r\n")
+			  .append("</html>\r\n");
+	}
+
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		String user = request.getParameter("Type");
+		
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		
+		// create HTML response
+		PrintWriter writer = response.getWriter();
+                 response.sendRedirect("newjsp.jsp");
+                
+                
+                
+                
+		writer.append("<!DOCTYPE html>\r\n")
+			  .append("<html>\r\n")
+			  .append("		<head>\r\n")
+			  .append("			<title>Welcome message</title>\r\n")
+			  .append("		</head>\r\n")
+			  .append("		<body>\r\n");
+		if (user != null && !user.trim().isEmpty()) {
+			writer.append("	Welcome " + user + ".\r\n");
+			writer.append("	You successfully completed this javatutorial.net example.\r\n");
+		} else {
+			writer.append("	You did not entered a name!\r\n");
+		}
+		writer.append("		</body>\r\n")
+			  .append("</html>\r\n");
+	}	
+
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
