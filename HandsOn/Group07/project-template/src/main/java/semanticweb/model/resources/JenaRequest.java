19c5350363c51f6c690890fcd0d0ef6bf08e001b
javier cabrera <user.javier.cabrera.marugan@alumnos.upm.es>
2019-11-02 20:29:36 +0100

    actualizado los cambios de rdf y jena

--- HandsOn/Group07/project-template/src/main/java/semanticweb/model/resources/JenaRequest.java
+++ HandsOn/Group07/project-template/src/main/java/semanticweb/model/resources/JenaRequest.java
@@ -1,57 +1,45 @@
 package semanticweb.model.resources;
-import org.apache.jena.ontology.*;
+
+import java.io.InputStream;
+
+import org.apache.jena.ontology.Individual;
+import org.apache.jena.ontology.OntClass;
+import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
-import org.apache.jena.query.Query;
-import org.apache.jena.query.QueryExecution;
-import org.apache.jena.query.QueryExecutionFactory;
-import org.apache.jena.query.QueryFactory;
-import org.apache.jena.query.QuerySolution;
-import org.apache.jena.query.ResultSet;
-import org.apache.jena.rdf.model.Literal;
-import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Property;
-import org.apache.jena.rdf.model.RDFNode;
-import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.rdf.model.Statement;
-import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.util.FileManager;
-import org.apache.jena.vocabulary.VCARD;
+import org.apache.jena.util.iterator.ExtendedIterator;
 
 public class JenaRequest {
     private static String ns = "http://www.semanticweb.org/grupo07/ontologies#";
-    //public static String pwl ="http://www.semanticweb.org/pc/ontologies/2018/9#";
-    public static String topo="http://data.ign.fr/def/topo#";
-    private static String rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#";
-    private static String owl="http://www.w3.org/1999/02/22-rdf-syntax-ns#";
-    private static String xsd="http://www.w3.org/2001/XMLSchema#";
-    private static String rdfs= "http://www.w3.org/2000/01/rdf-schema#";
+    public static String topo = "http://data.ign.fr/def/topo#";
+    private static String rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+    private static String owl = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+    private static String xsd = "http://www.w3.org/2001/XMLSchema#";
+    private static String rdfs = "http://www.w3.org/2000/01/rdf-schema#";
     private static String stringTypeURI = "http://www.w3.org/2001/XMLSchema#string";
-    private static String schema="http://schema.org/";
-    private static Model model = ModelFactory.createDefaultModel();
+    private static String schema = "http://schema.org/";
+    private static String lat=  "<http://www.semanticweb.org/grupo07/ontologies/property#hasLatitude>";
+    private static String lon= "<http://www.semanticweb.org/grupo07/ontologies/property#hasLongitud>";
+    private static  OntModel model = ModelFactory.createOntologyModel(OntModelSpec.RDFS_MEM_RDFS_INF);
 
-    public static Park ParkRequest(String latitude, String longitude){
 
-        String queryString =
-                        "\"PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>\"+\n" +
-                        "\"PREFIX latitude: <http://www.semanticweb.org/grupo07/ontologies/property#hasLatitude>\"\n" +
-                        "\"PREFIX longitud: <http://www.semanticweb.org/grupo07/ontologies/property#hasLongitud>\"\n" +
-                        "\"PREFIX nombre: <http://www.semanticweb.org/grupo07/ontologies/property#hasName>\"\n" +
-                        "\"PREFIX descripcion: <http://www.semanticweb.org/grupo07/ontologies/property#hasDescription> \"\n" +
-                        "\"PREFIX metro: <http://www.semanticweb.org/grupo07/ontologies/property#hasUnderground>\"\n" +
-                        "\"PREFIX dbo: <http://dbpedia.org/ontology/>\" +\n" +
-                        "                    \"SELECT ?name ?descripcion ?metro WHERE {\" +\n" +
-                        "                    \"  ?s geo:lat ?lat .\" +\n" +
-                        "                    \"  ?s geo:long ?long .\" +\n" +
-                        "                    \"}\" ); ";
-        Query query = QueryFactory.create(queryString);
-        QueryExecution qexec = QueryExecutionFactory.create(query, model) ;
-        ResultSet results = qexec.execSelect() ;
-        Park park= new Park();
-        park.setName(results.next().getLiteral("name").getString());
-        park.setDescription(results.next().getLiteral("description").getString());
-        park.setTransport(results.next().getLiteral("transport").getString());
+    public static void main(String[] args) {
+        String filename = "/home/javier/universidad/4º curso/semantic_web_and_linked_data/curso2019-2020/Curso2019-2020/HandsOn/Group07/project-template/src/main/java/semanticweb/model/resources/parques-jardines-with-links.ttl";
+        OntModel model = ModelFactory.createOntologyModel(OntModelSpec.RDFS_MEM_RDFS_INF);
+        InputStream in = FileManager.get().open(filename);
+        if (in == null)
+            throw new IllegalArgumentException("File: "+filename+" not found");
 
-        return  park;
+        model.read(in, null);
+        OntClass parkclass = model.getOntClass(ns+"Park");
+        ExtendedIterator parkInstances = parkclass.listInstances();
+
+        int i=0;
+        while (parkInstances.hasNext())
+        {
+            Individual instance = (Individual) parkInstances.next();
+            System.out.println("Instance of Person "+ ++i +": "+instance.getURI());
+        }
     }
 }
