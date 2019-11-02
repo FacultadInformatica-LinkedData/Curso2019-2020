9f6c72ec9cff0eecf1752f13127e4da6b46d6003
Pablo Castillo <pablo.castillo.martinez@alumnos.upm.es>
2019-11-02 19:59:15 +0100

    Añadido clase para tratar arboles

--- HandsOn/Group07/project-template/src/main/java/semanticweb/model/resources/Tree.java
+++ HandsOn/Group07/project-template/src/main/java/semanticweb/model/resources/Tree.java
@@ -1,5 +1,50 @@
 package semanticweb.model.resources;
 
 public class Tree {
+    private String name;
+    private String description;
+    private String clase;
+    private String division;
+    private String family;
+    private String quantity;
 
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getClase() {
+        return clase;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setClase(String clase) {
+        this.clase = clase;
+    }
+
+    public void setDivision(String division) {
+        this.division = division;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
 }
