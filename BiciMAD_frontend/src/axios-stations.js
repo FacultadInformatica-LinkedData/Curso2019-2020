93a0d674580b05f09e16914574cce2404edd8075
riverita98 <riverarmd98@gmail.com>
2019-11-12 19:34:26 +0100

    Ficheros externos eliminados

--- BiciMAD_frontend/src/axios-stations.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import { BACKEND_HOST } from './config/env';
-
-const instance = axios.create({
-    baseURL: BACKEND_HOST,
-})
-
-export default instance;
--- BiciMAD_frontend/src/config/env.js
+++
@@ -1,1 +0,0 @@
-export const BACKEND_HOST = process.env.BACKEND_HOST || 'http://localhost:8080/';
